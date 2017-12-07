f68589343a21ee1fc8df31f91d1cf6f002621d82
Anthony Mengel <anthonymengel1232@gmail.com>
2017-12-07 14:22:14 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+      JWT_SECRET: process.env.JWT_SECRET,
+      PORT: process.env.PORT || 8080,
+      CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'mongodb://localhost/mealmatcherdb',
+      DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/mealmatcherdb',
+      TEST_DATABASE_URL: 
+      process.env.TEST_DATABASE_URL ||
+      'mongodb://localhost/mealmatcherdb',
+      JWT_EXPIRY: process.env.JWT_EXPIRY,
+};
---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const { PORT, CLIENT_ORIGIN } = require('./config');
+const { dbConnect } = require('./db-mongoose');
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const passport = require('passport');
+
+//passport.use(jwtStrategy);
+const app = express();
+// endpoints
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Request-Headers");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(
+    morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+        skip: (req, res) => process.env.NODE_ENV === 'test'
+    })
+);
+
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
+
+
+
+
+function runServer(port = PORT) {
+    const server = app
+        .listen(port, () => {
+            console.info(`App listening on port ${server.address().port}`);
+        })
+        .on('error', err => {
+            console.error('Express failed to start');
+            console.error(err);
+        });
+}
+
+if (require.main === module) {
+    dbConnect();
+    runServer();
+}
+
+module.exports = { app };
