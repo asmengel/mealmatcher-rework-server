af3614051024352940fbcac16e5ebc01497d3417
Anthony Mengel <anthonymengel1232@gmail.com>
2018-01-04 18:06:07 -0500

    added more functionality to endpoints to recieve data from api

--- restaurant/router.js
+++ restaurant/router.js
@@ -7,10 +7,14 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 router.use(jsonParser);
 const { basicAuth, jwtAuth } = require('../auth/strategies');
-router.get('/reservations/:id', (req, res) => {
+router.get('/info/:id', (req, res) => {
     return Restaurant.findOne({RestaurantId: req.params.id})
         .then(restaurant => {
+            if(!restaurant){
+                return res.sendStatus(404)
+            }
             return res.status(200).json(restaurant.apiRepr())
+
         })
         .catch(err => {
             console.log(err)
@@ -23,36 +27,41 @@ router.get('/reservations/:id', (req, res) => {
 //     })
 // })
 // not sure why this does not work $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-router.post('/reservations/:id', jwtAuth, (req, res) => {
+//router.post('/reservations/:id', jwtAuth, (req, res) => {
     // console.log('endpoint hit', req)
 
-    let restaurant = {};
-    //let { RestaurantName, HasReservations, UsersInterested, NumberOfReservations} = restaurant;
-    console.log(restaurant);
-    return Restaurant.create({
-        RestaurantName: req.body.RestaurantName, HasReservations: req.body.HasReservations,
-        NumberOfReservations: req.body.NumberOfReservations, UsersInterested: [req.user._id], RestaurantId: req.body.RestaurantId
-    })
+//     let restaurant = {};
+//     //let { RestaurantName, HasReservations, UsersInterested, NumberOfReservations} = restaurant;
+//     console.log(restaurant);
+//     return Restaurant.create({
+//         RestaurantName: req.body.RestaurantName, HasReservations: req.body.HasReservations,
+//         NumberOfReservations: req.body.NumberOfReservations, UsersInterested: [req.user._id], RestaurantId: req.body.RestaurantId
+//     })
 
-        .then(restaurant => {
-            console.log(restaurant, 'does the promise fire');
-            return res.status(201).json(restaurant.apiRepr());
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({ code: 500, message: 'internal server error' });
-        });
-});
+//         .then(restaurant => {
+//             console.log(restaurant, 'does the promise fire');
+//             return res.status(201).json(restaurant.apiRepr());
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json({ code: 500, message: 'internal server error' });
+//         });
+// });
 // /join/:id
-router.get('/reservations/:id', jwtAuth, (req, res) => {
+router.post('/reservations/:id', jwtAuth, (req, res) => {
     Restaurant.findOneAndUpdate({
-        _id: req.params.id
+        RestaurantId: req.params.id
     }, {
+            $set: req.body,
             $addToSet: { UsersInterested: req.user._id }
+        }, 
+        {
+            upsert:true
         }).then(restaurant => {
             res.json(restaurant)
         }).catch(err => {
             res.status(500).json(err)
+            console.log(err);
         })
 })
 
